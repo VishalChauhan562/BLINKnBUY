2b7f71f03a9942524ea863c981706bddde801348
Vishal Chauhan <vishalchauhan@192.168.0.196>
2023-01-26 13:42:41 +0530

    product-list-page-completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,10 +28,12 @@ import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { RoutesWithUserChatComponent } from "./components/user/RoutesWithUserChatComponent";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <HeaderComponent />
       <Routes>
         <Route element={<RoutesWithUserChatComponent />}>
